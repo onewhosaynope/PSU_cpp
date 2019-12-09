d15e4f29a5045301044bf8c8a30674bbfd4cbb2a
Vsevolod <onewhosaynope@gmail.com>
2019-12-09 13:03:04 +0300

    matrix template

--- templates/matrix.cpp
+++ templates/matrix.cpp
@@ -1,86 +1,69 @@
 #include <iostream>
-//#include <clocale>
 using namespace std;
-//----- объявление прототипов используемых функций-----------------
-int** Create(int n,int m);           // создание матрицы n строк m столбцов
-void Free(int** M,int n);             // освобождение матрицы
-void Input(int** M,int n,int m);   // ввод матрицы
-void Print(int** M,int n,int m);     // вывод матрицы
-//--------------------------------------------------------------------------
-void Work(int** M,int n,int m);    // обработка матрицы
-//   Сюда добавить протопипы тех функций которые дополнительно напишете
-//--------------------------------------------------------------------------
- 
-//----------  main() -  точка входа в программу консольного приложения 
-int main()
-{
-    //setlocale(LC_ALL,"Rus");
-    int n,m;
-    cout<<"Количество строк матрицы:?";
-    cin>>n;
-    cout<<"Количество столбцов матрицы:?";
-    cin>>m;
-    int** A=Create(n,m);
-    Input(A,n,m);
-    Work(A,n,m);
-    Free(A,n);
-    //system("pause");
+
+int** matrix_init(int n, int m);
+void matrix_clean(int** M, int n);
+void matrix_fill(int** M, int n, int m);
+void matrix_print(int** M, int n, int m);
+void solution(int** M, int n, int m);
+
+int main() {
+    int n, m;
+    cout << "Number of lines [n]: ";
+    cin >> n;
+    cout << "Number of colums: [m]";
+    cin >> m;
+    int** A = matrix_init(n, m);
+    matrix_fill(A, n, m);
+    solution(A, n, m);
+    matrix_clean(A, n);
     return 0;
 }
  
-//----   описание функций -----------------------------------------
-int** Create(int n,int m)
-{
-    int** M=new int*[n];
-    for(int i=0;i<n;i++)
-    {
-        M[i]=new int[m];
+
+int** matrix_init(int n, int m) {
+    int** M = new int*[n];
+    for(int i = 0; i < n; i++) {
+        M[i] = new int[m];
     }
     return M;
 }
-//----------------------------
-void Free(int** M,int n)
-{
-    for(int i=0;i<n;i++)
+
+
+void matrix_clean(int** M, int n) {
+    for(int i = 0; i < n; i++)
         delete[] M[i];
     delete[] M;
 }
-//----------------------------
-void Input(int** M,int n,int m)
-{
-    for(int i=0;i<n;i++)
-    {
-        for(int j=0;j<m;j++)
-        {
-            cout<<"M["<<i<<"]["<<j<<"]=?";
+
+
+void matrix_fill(int** M, int n, int m) {
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            cout << "M[" << i << "][" << j << "] : ";
             cin >> M[i][j];
         }
     
     }
 }
-//----------------------------
-void Print(int** M,int n,int m)
-{
-    for(int i=0;i<n;i++)
-    {
-        for(int j=0;j<m;j++)
-        {
+
+
+void matrix_print(int** M, int n, int m)    {
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
             cout << M[i][j] << " ";
         }
         cout << endl;
     }
 }
-// пример обработки - подсчет сумм в каждой строке
-void Work(int** M,int n,int m)
-{
-    for(int i=0;i<n;i++)
-    {
-        int sum=0;
-        for(int j=0;j<m;j++)
-        {
-             sum+=M[i][j];
+
+
+void solution(int** M, int n, int m) {
+    for(int i = 0; i < n; i++) {
+        int sum = 0;
+        for(int j = 0; j < m; j++) {
+             sum += M[i][j];
         }
-        cout<<i<<" sum = " << sum<< endl;
+        cout << i << " sum = " << sum << endl;
     }
 }
-// сюда вставить все дополнительные функции которые напишете
