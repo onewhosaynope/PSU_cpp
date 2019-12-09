e5e06be7c83ece1efa4fb452ec286d78269a63c8
Vsevolod <onewhosaynope@gmail.com>
2019-12-09 12:32:25 +0300

    fixed templates

---
+++ lab6/task_2.cpp
@@ -0,0 +1,73 @@
+/**
+ * Пазов В.С.
+ * Matrix57
+ * Дана матрица размера M×N (M и N — четные числа). 
+ * Поменять местами левую верхнюю и правую нижнюю четверти матрицы.
+ */
+
+
+#include <iostream>
+using namespace std;
+
+
+int** matrix_init(int n, int m);
+void matrix_clean(int** M, int n);
+void matrix_fill(int** M, int n, int m);
+void matrix_print(int** M, int n, int m);
+void Solution(int** M, int n, int m);
+
+
+int main() {
+    int n, m;
+    cout << "Number of lines for matrix:" << endl;
+    cin >> n;
+    cout << "Number of columns for matrix:";
+    cin >> m;
+    int** A = matrix_init(n, m);
+    matrix_fill(A, n, m);
+    Solution(A, n, m);
+    matrix_clean(A, n);
+    return 0;
+}
+
+
+int** matrix_init(int n, int m) {
+    int** M = new int*[n];
+    for(int i = 0; i < n; i++) {
+        M[i] = new int[m];
+    }
+    return M;
+}
+
+
+void matrix_clean(int** M, int n) {
+    for(int i = 0; i < n; i++)
+        delete[] M[i];
+    delete[] M;
+}
+
+
+void matrix_fill(int** M, int n, int m) {
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            cout << "M[" << i << "][" << j << "]=?";
+            cin >> M[i][j];
+        }
+    
+    }
+}
+
+
+void matrix_print(int** M, int n, int m) {
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            cout << M[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+
+void Solution(int** M, int n, int m) {
+    
+}
--- templates/vector.cpp
+++ templates/vector.cpp
@@ -1,70 +1,53 @@
 #include <iostream>
-//#include <clocale>
 #include <cstdlib>
 
 using namespace std;
 
-//----- объявление прототипов используемых функций----------------------------------------
-void Input(int* M,int n);  // ввод массива
-void Print(int* M,int n);   // вывод массива
-void RandomFill(int* M,int n);   // заполнение массива случайными числами
-//--------------------------------------------------------------------------
-int Work(int* M,int n);   // пример обработки массива
-//   Сюда добавить протопипы тех функций которые дополнительно напишете
-//--------------------------------------------------------------------------
- 
-//----------  main() -  точка входа в программу консольного приложения 
-int main()
-{
-    //setlocale(LC_ALL,"Rus");
+void array_fill(int* M,int n);  // ввод массива
+void array_print(int* M,int n);   // вывод массива
+void array_rand(int* M,int n);   // заполнение массива случайными числами
+int solution(int* M,int n);   // пример обработки массива
+
+
+int main() {
     int n;
-    cout<<"Размер вектора?";
-    cin>>n;
-    int* A=new int[n];
-    //Input(A,n);
-    RandomFill(A,n);
-    Print(A,n); 
-    int s=Work(A,n);
-    cout<<"Summa = " << s<< endl;
+    cout << endl << "Array size [N]:" << endl;
+    cin >> n;
+    int* A = new int[n];
+    array_fill(A, n);
+    array_rand(A, n);
+    array_print(A, n); 
+    int result = solution(A, n);
+    cout << "Summa = " << result << endl;
     delete[] A;
-    //system("pause");
     return 0;
 }
-//----   описание функций -----------------------------------------
-// ввод массива
-void Input(int* M,int n)
-{
-    for(int i=0;i<n;i++)
-    {
-        cout<<"M["<<i<<"]=?";
+
+
+void array_fill(int* M, int n) {
+    for(int i = 0; i < n; i++) {
+        cout << "M[" << i << "]";
             cin >> M[i];
     }
 }
-// заполнение случайными числами
-void RandomFill(int* M,int n)
-{
-    for(int i=0;i<n;i++)
+
+
+void array_rand(int* M, int n) {
+    for(int i = 0; i < n; i++)
     {
-        M[i]=rand()%100;
+        M[i] = rand() % 100;
     }
 }
-  // вывод массива
-void Print(int* M,int n)
-{
-    for(int i=0;i<n;i++)
-    {
+
+
+void array_print(int* M, int n) {
+    for(int i = 0; i < n; i++) {
         cout << M[i]<< " ";
     }
     cout << endl;
 }
-// пример обработки - подсчет суммы элементов
-int Work(int* M,int n)
-{
-    int sum=0;
-    for(int i=0;i<n;i++)
-    {
-        sum+=M[i];
-    }
-    return sum;
-}
-// сюда вставить все дополнительные функции которые напишете
+
+
+void solution(int M[], int n, int k) {
+    // work with array here
+} 
