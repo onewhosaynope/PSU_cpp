42f65742be012ec18fc98cf48061145cfb91419d
Vsevolod <onewhosaynope@gmail.com>
2019-12-16 23:47:20 +0300

    updated lab7

--- lab7[ready]/task_1.cpp
+++ lab7[ready]/task_1.cpp
@@ -2,27 +2,62 @@
  * Пазов В.С.
  * String36 (работа со строкой)
  * Даны  строкиS, S1 и S2.
- * Заменить в строке S первое  вхождение
+ * Заменить в строке S вхождения
  * строки S1 на строкуS2.
  */
 
 
 #include <iostream>
 #include <string>
+
 using namespace std;
- 
-int main(){
-    string S, S1, S2;
-    cout << "Enter S: " << endl;
-    getline(cin, S);
-    cout << "Enter S1: " << endl;
-    getline(cin, S1);
-    cout << "Enter S2: " << endl;
-    getline(cin, S2);
 
-    // http://www.cplusplus.com/reference/string/string/replace/
-    S.string::replace(S.string::find(S1), S1.string::length(), S2);
-
-    cout << S;
-return 0;
+void replace_all( string &S, const string &S1, const string &S2 )
+{
+    for (string::size_type pos = 0; ( pos = S.find( S1, pos ) ) != string::npos; pos += S2.size()) {
+        S.replace( pos, S1.size(), S2 );
+    }
 }
+
+int main() 
+{
+    string S( "Hello world, world, world" );
+    string S1( "world" );
+    string S2( "mccdlibby" );
+
+    cout << S << endl;
+
+    replace_all( S, S1, S2 );
+
+    cout << S << endl;
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+// #include <iostream>
+// #include <string>
+// using namespace std;
+ 
+// int main(){
+//     string S, S1, S2;
+//     cout << "Enter S: " << endl;
+//     getline(cin, S);
+//     cout << "Enter S1: " << endl;
+//     getline(cin, S1);
+//     cout << "Enter S2: " << endl;
+//     getline(cin, S2);
+
+//     // http://www.cplusplus.com/reference/string/string/replace/
+//     S.string::replace(S.string::find(S1), S1.string::length(), S2);
+
+//     cout << S;
+// return 0;
+// }
--- lab7[ready]/task_2.cpp
+++ lab7[ready]/task_2.cpp
@@ -1,40 +1,34 @@
 /**
  * Пазов В.С.
  * String36 (работа с массивом символов)
- * Даны  строкиS, S1 и S2.
- * Заменить в строке S первое  вхождение
- * строки S1 на строкуS2.
+ * Даны  строкиS, S и S1.
+ * Заменить в строке S вхождения
+ * строки S на строкуS2.
  */
 
-
 #include <iostream>
-#include <sstream>
 #include <string>
-#include <list>
-#include <algorithm>
-#include <iterator>
 
 using namespace std;
- 
-int main(){
-    string S, S1, S2;
 
-    cout << "S: ";
-    getline(cin, S);
-    cout << "S1: ";
-    cin >> S1;
-    cout << "S2: ";
-    cin >> S2;
+void replace_all( string &S, const string &S1, const string &S2 )
+{
+    for (string::size_type pos = 0; ( pos = S.find( S1, pos ) ) != string::npos; pos += S2.size()) {
+        S.replace( pos, S1.size(), S2 );
+    }
+}
 
-    istringstream ist(S); // https://en.cppreference.com/w/cpp/io/basic_istringstream
-    list<string> list; // https://ru.cppreference.com/w/cpp/container/list
+int main() 
+{
+    string S( "Hello world, world, world" );
+    string S1( "world" );
+    string S2( "mccdlibby" );
 
-    copy(istream_iterator<string>(ist), istream_iterator<string>(), back_inserter(list));
+    cout << S << endl;
 
-    replace(list.begin(), list.end(), S1, S2);
+    replace_all( S, S1, S2 );
 
-    cout << "Result: ";
-    copy(list.begin(), list.end(), ostream_iterator<string>(cout, " "));
+    cout << S << endl;
 
     return 0;
 }
